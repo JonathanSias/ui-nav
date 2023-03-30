dec61c3db7baba96088b261420cd22948bb49e94
Jonathan Sias <jonathancsias@gmail.com>
2023-03-30 17:51:33 -0300

    First page element created: Menu.
    Global menu with a dock style was created. The colors were not defined yet, so the colors used can be updated in the future. Also the dark mode button was created and is it working fine.

---
+++ main.js
@@ -0,0 +1,15 @@
+let theme = document.querySelector('.toggle-theme');
+
+theme.addEventListener('click', function() {
+    if (document.body.classList.contains('light-mode')) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+    }
+});
+
+// Bootstrap Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
